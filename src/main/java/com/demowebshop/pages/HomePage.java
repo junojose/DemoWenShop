2ea60856b9322e2930f07c1eaab407dc13810d82
junojose <junojose91@gmail.com>
2023-04-06 08:41:28 +0530

    adding home page fields

--- src/main/java/com/demowebshop/pages/HomePage.java
+++ src/main/java/com/demowebshop/pages/HomePage.java
@@ -1,5 +1,52 @@
 package com.demowebshop.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.demowebshop.utilities.RandomUtility;
+import com.demowebshop.utilities.TestHelperUtility;
+import com.demowebshop.utilities.WaitUtility;
+
+public class HomePage extends TestHelperUtility {
+	public WebDriver driver;
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	private final String _subEmailField = "newsletter-email";
+	@FindBy(id = _subEmailField)
+	private WebElement subEmailField;
+	private final String _subButton = "newsletter-subscribe-button";
+	@FindBy(id = _subButton)
+	private WebElement subButton;
+	private final String _subMessage = "newsletter-result-block";
+	@FindBy(id = _subMessage)
+	private WebElement subMessage;
+
+	public String getHomePageTitle() {
+		String title = page.getPageTitle(driver);
+		return title;
+	}
+
+	public void enterSubEmail() {
+		String emailId = RandomUtility.getRandomEmail();
+		page.enterText(subEmailField, emailId);
+
+	}
+	public void clickOnSubButton()
+	{
+		page.clickOnElement(subButton);
+	}
+	public String getSubMessage()
+	{
+		wait.setHardWait();
+		wait.waitForElementToBeVisible(driver, _subMessage,WaitUtility.LocatorType.Id);
+		String message=page.getElementText(subMessage);
+		return message;
+	}
 
 }
