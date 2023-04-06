58ec67e3ccc10fdaa63a86ade90da4444a6b9c2b
junojose <junojose91@gmail.com>
2023-04-06 08:40:20 +0530

    adding errormessages

--- src/main/java/com/demowebshop/constants/ErrorMessages.java
+++ src/main/java/com/demowebshop/constants/ErrorMessages.java
@@ -1,5 +1,6 @@
 package com.demowebshop.constants;
 
 public class ErrorMessages {
-
+	public static final String TITLE_FAILURE_MESSAGES="Error: Invalid Title Found";
+	public static final String EMAIL_FAILURE_MESSAGES="Error: Invalid Email Message Found";
 }
