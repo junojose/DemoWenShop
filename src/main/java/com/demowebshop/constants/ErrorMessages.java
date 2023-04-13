e4462e417342b56dc0091c816957d262f68fad44
junojose <junojose91@gmail.com>
2023-04-13 18:46:42 +0530

    commit all

--- src/main/java/com/demowebshop/constants/ErrorMessages.java
+++ src/main/java/com/demowebshop/constants/ErrorMessages.java
@@ -3,4 +3,6 @@ package com.demowebshop.constants;
 public class ErrorMessages {
 	public static final String TITLE_FAILURE_MESSAGES="Error: Invalid Title Found";
 	public static final String EMAIL_FAILURE_MESSAGES="Error: Invalid Email Message Found";
+	public static final String INVALID_USERNAME_PASSWORD_MESSAGES="Invalid UserName/Password";
+	public static final String PASSWORD_AND_USERNAME_MATCHES="UserName and Password Matches";
 }
