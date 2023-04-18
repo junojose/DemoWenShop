dd768496e5e7148d07447337ac63889821b3afa7
junojose <junojose91@gmail.com>
2023-04-18 12:11:37 +0400

    commit

---
+++ src/main/java/com/demowebshop/constants/ExtentLogMessage.java
@@ -0,0 +1,8 @@
+package com.demowebshop.constants;
+
+public class ExtentLogMessage {
+	public static final String HOME_PAGE_TITLE_RECEIVED="Home title is received";
+	public static final String ACTUAL_AND_EXPECTED_MESSAGE_VALIDATE="Validation is Succesful";
+	public static final String EMAIL_SUBSCRIBE_BUTTON_VALIDATE="Email subscription validate";
+	public static final String MAIL_SUBCRIPTION_FOUND="Subscribe the mail";
+}
