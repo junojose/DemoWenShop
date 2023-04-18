320322d0b970e261ed44966a0e9a35a7d0311c91
junojose <junojose91@gmail.com>
2023-04-18 12:06:38 +0400

    commit

--- src/main/java/com/demowebshop/pages/UserAccountPage.java
+++ src/main/java/com/demowebshop/pages/UserAccountPage.java
@@ -1,5 +1,33 @@
 package com.demowebshop.pages;
 
-public class UserAccountPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.demowebshop.utilities.TestHelperUtility;
+
+public class UserAccountPage extends TestHelperUtility {
+	public WebDriver driver;
+
+	public UserAccountPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	private final String _validationmail = "//div[@class='header-links']//a[@class='account']";
+	@FindBy(xpath = _validationmail)
+	private WebElement validationmail;
+	private final String _useremail = "//a[text()='jun123@gmail.com']";
+	@FindBy(xpath = _useremail)
+	private WebElement useremail;
+
+	public String getusermail() {
+		String email = page.getElementText(validationmail);
+		return email;
+	}
+	public String getuseremail() {
+		String uemail = page.getElementText(useremail);
+		return uemail;
+	}
 }
